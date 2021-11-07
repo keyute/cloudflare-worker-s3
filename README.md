2863f0dfcdeb8366d0ebb19800b1af8820d29b2d
Low Xuguang <personal@keyute.io>
2021-11-07 18:30:37 +0800

    init

---
+++ README.md
@@ -0,0 +1,55 @@
+# Cloudflare Worker S3
+
+## Wrangler
+
+To generate using [wrangler](https://github.com/cloudflare/wrangler)
+
+```
+wrangler generate projectname https://github.com/keyute/cloudflare-worker-s3.git
+```
+
+## Configure
+
+### KV
+Workers KV is the preferred method to access credentials as it supports exceptionally high read volumes and allows high configurability by binding the worker to different KVs based on routes.
+
+These values are required and has no defaults. Any of these values not configured will result in an exception.
+
+| Key | Description |
+| --- | --- |
+| AWS_ACCESS_KEY_ID | AWS Access Key of user with permission to access your bucket |
+| AWS_REGION | AWS region where your bucket is hosted |
+| AWS_S3_BUCKET | Name of your bucket |
+| AWS_SECRET_ACCESS_KEY | AWS Secret Key of user with permission to access your bucket |
+| PRIVATE_KEY (optional) | Your base64 encoded password |
+| PRIVATE_REGEX (optional) | Regex of your private file paths for verification |
+
+### Environment
+Add these lines to the bottom of [`wrangler.toml`](https://github.com/keyute/cloudflare-worker-s3/blob/main/wrangler.toml) and replace empty values as needed.
+
+```
+[env.your_env]
+zone_id = ""
+route = ""
+kv_namespaces = [
+    { binding = "KV", id = "" }
+]
+```
+
+You can add as many environments as you want and publish to these environments respectively.
+
+## Usage
+
+### Verify
+Call private files with the parameter ```token``` in your URL with the format ```<timestamp>_<sha256_signature>```
+
+### Generate
+Generate a SHA256 signature with the same ```SIGNATURE_KEY``` with the format ```<path><timestamp>```
+
+## Publish
+
+To publish using [wrangler](https://github.com/cloudflare/wrangler)
+
+```
+wrangler publish --env [your_env]
+```
---
+++ index.js
@@ -0,0 +1,55 @@
+import { AwsClient } from 'aws4fetch'
+
+addEventListener('fetch', function(event) {
+    event.respondWith(handleRequest(event.request))
+});
+
+async function handleRequest(request) {
+    let url = new URL(request.url);
+    const privatePath = await KV.get("PRIVATE_REGEX")
+    if (privatePath !== null && new RegExp(privatePath).test(url.pathname)) {
+        const token = url.searchParams.get("token")
+        if (token === null || !/^(\d{10,11})_([a-f0-9]{64})$/.test(token)) {
+            return new Response("Invalid token format", { status: 403 })
+        }
+        const [expiration, signature] = token.split('_')
+        if (Date.now() / 1000 > parseInt(expiration)) {
+            return new Response("Token has expired", { status: 410 })
+        }
+        const key = await crypto.subtle.importKey(
+            "raw",
+            base64ToArrayBuffer(await KV.get("PRIVATE_KEY")),
+            { name: "HMAC", hash: "SHA-256"},
+            false,
+            ["verify"]
+        )
+        const verified = await crypto.subtle.verify("HMAC", key, hexStringToUint8Array(signature), new TextEncoder().encode(url.pathname + expiration))
+        if (!verified) {
+            return new Response("Token verifiation failed", { status: 403 })
+        }
+    }
+    const aws = new AwsClient({
+        "accessKeyId": await KV.get("AWS_ACCESS_KEY_ID"),
+        "secretAccessKey": await KV.get("AWS_SECRET_ACCESS_KEY"),
+    });
+    url.hostname = await KV.get("AWS_S3_BUCKET") + ".s3." + await KV.get("AWS_REGION") + ".amazonaws.com";
+    let signedRequest = await aws.sign(url);
+    return await fetch(signedRequest, { "cf": { "cacheEverything": true } });
+}
+
+function hexStringToUint8Array(hexString) {
+    let arrayBuffer = new Uint8Array(hexString.length / 2);
+    for (let i = 0; i < hexString.length; i += 2) {
+        arrayBuffer[i/2] = parseInt(hexString.substr(i, 2), 16);
+    }
+    return arrayBuffer;
+}
+
+function base64ToArrayBuffer(base64) {
+    let binaryString = atob(base64)
+    let bytes = new Uint8Array(binaryString.length)
+    for (let i = 0; i < binaryString.length; i++) {
+        bytes[i] = binaryString.charCodeAt(i)
+    }
+    return bytes.buffer
+}
